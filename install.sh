c852f65eca0e12f7dae813b1fc1a36a5c62ff483
walksanatora <walkerffo22@hotmail.com>
2022-04-04 13:46:27 -0700

    verbosity

--- install.sh
+++ install.sh
@@ -87,6 +87,6 @@ echo "copying x-qoi.xml to /usr/local/share/mime/packages"
 cp x-qoi.xml "/usr/local/share/mime/packages"
 echo "updateing mime database"
 update-mime-database "/usr/local/share/mime"
-echo "installing magic number"
+echo "installing magic number (so 'qoi' images can be picked up via the 'file' command)"
 echo "0 string qoif image/x-qoi" >> /etc/magic
 echo "Done"
